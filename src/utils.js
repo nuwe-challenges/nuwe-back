0df256771c369bf33d7a810127acde6009792829
Javi Vañó <javibodi@gmail.com>
2022-02-10 17:53:03 +0100

    validation configuration

---
+++ src/utils.js
@@ -0,0 +1,12 @@
+module.exports = {
+    validateEmail: (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            );
+    },
+    validatePhoneNumber: (phone) => {
+        return String(phone).match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
+    },
+};
